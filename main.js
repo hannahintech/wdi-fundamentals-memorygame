9945dd980ad8f7d250a154804fe6118199204a92
Hannah Tucker <Hannah@Andreas-Air.home>
2017-11-06 17:22:50 +0000

    more game logic

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "wdi-memorygame-imgs/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "wdi-memorygame-imgs/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "wdi-memorygame-imgs/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "wdi-memorygame-imgs/king-of-diamonds.png",
+	},
+
+]
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king");
-if (cardsInPlay.length === 2) {
+
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again");
 	}
 }
-//var cardThree = cards[1];
-//var cardFour = cards[3];
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+	}
+}
+
+flipCard(0);
+flipCard(2);
+console.log(cardsInPlay);
