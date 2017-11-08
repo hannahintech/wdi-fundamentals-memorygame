af5e66f44667a311016f25a8013326fba6606aae
Hannah Tucker <Hannah@Andreas-Air.home>
2017-11-08 12:58:37 +0000

    finished game logic

--- main.js
+++ main.js
@@ -33,9 +33,11 @@ var checkForMatch = function(){
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', (cards[cardId].cardImage));
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 		console.log(cards[cardId].cardImage);
@@ -43,6 +45,14 @@ var flipCard = function(cardId){
 	}
 }
 
-flipCard(0);
-flipCard(2);
-console.log(cardsInPlay);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'wdi-memorygame-imgs/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
